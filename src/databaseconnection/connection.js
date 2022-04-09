d95f16a31ca0351f4412d701fda0654c92204baf
ved <vedprakashkushwaha94@gmail.com>
2022-04-09 16:36:32 +0530

    add comment

--- src/databaseconnection/connection.js
+++ src/databaseconnection/connection.js
@@ -1,4 +1,4 @@
-
+//comment added in from registration  branch
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/register-Api", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("connection successfully..... "))
